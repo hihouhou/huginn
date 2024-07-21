800ad16401c435563d621312d5e98b0e84d61eea
Nicolas Germain <ngermain@hihouhou.com>
2024-07-22 01:20:29 +0200

    test dropbox

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -250,7 +250,7 @@ Devise.setup do |config|
   if defined?(OmniAuth::Strategies::DropboxOauth2) &&
      (key = ENV["DROPBOX_OAUTH_KEY"]).present? &&
      (secret = ENV["DROPBOX_OAUTH_SECRET"]).present?
-    config.omniauth :dropbox, key, secret, strategy_class: OmniAuth::Strategies::DropboxOauth2, request_path: '/auth/dropbox', callback_path: '/auth/dropbox/callback, request_method: :post
+    config.omniauth :dropbox, key, secret, strategy_class: OmniAuth::Strategies::DropboxOauth2, request_path: '/auth/dropbox', callback_path: '/auth/dropbox/callback request_method: :post
   end
 
   if defined?(OmniAuth::Strategies::Evernote) &&
